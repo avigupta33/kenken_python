841fb5b8e783ce010be10d349333797a595da601
Avi Gupta <avigupta7@gmail.com>
2020-06-11 12:31:25 -0700

    Deleted legacy code

--- cage.py
+++ cage.py
@@ -9,24 +9,12 @@ class Cage:
         self.goal = goal
         self.squares = squares
 
+    def getData(self, row_i, c):
+
 
     def verify(self) -> bool:
         if self.operator = "x":
 
 
-    def verifyValue(self, value: int) -> bool:
-        for row in self.data:
-            for val in row:
-                if val == value:
-                    return False
-        return True
 
-    def isInCage(self, row_i: int, col_i: int) -> bool:
-        if self.top_left[0]<=row_i<self.top_left[0]+self.cage_size:
-            if self.top_left[1]<=col_i<self.top_left[1]+self.cage_size:
-                return True
-        return False
-
-    def setFromAbsolute(self, row_i: int, col_i: int, val: int) -> None:
-        self.data[row_i - self.top_left[0]][col_i - self.top_left[1]] = val
 
