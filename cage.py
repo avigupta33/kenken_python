ea6556338f529ecbb6b21cc583dc6a688bc9332b
Avi Gupta <avigupta7@gmail.com>
2020-06-11 12:53:56 -0700

    Finished verify cage, added simple test cases

--- cage.py
+++ cage.py
@@ -13,9 +13,38 @@ class Cage:
         return self.board.getValue(coords[0], coords[1])
 
     def verify(self) -> bool:
-        if self.operator == "x":
+        if self.operator == "g":
+            if len(self.squares) > 1:
+                return False
+            else:
+                return self.getValue(self.squares[0]) == self.goal
+
+        if self.operator == "+":
+            product = reduce((lambda x, y: x+y), map(self.getValue, self.squares))
+            return product == self.goal
+
+        if self.operator == "*":
             product = reduce((lambda x, y: x*y), map(self.getValue, self.squares))
             return product == self.goal
+
+        if self.operator == "-":
+            if len(self.squares) !=  2:
+                return False
+            else:
+                diff = reduce((lambda x, y: abs(x-y)), map(self.getValue, self.squares))
+                return diff == self.goal
+
+        if self.operator == "/":
+            if len(self.squares) != 2:
+                return False
+            else:
+                q1 = reduce((lambda x, y: x/y), map(self.getValue, self.squares))
+                q2 = reduce((lambda x, y: y/x), map(self.getValue, self.squares))
+
+                return max(q1, q2) == self.goal
+
+
+
 
 
 
--- kenken.py
+++ kenken.py
@@ -6,7 +6,26 @@ if __name__ == "__main__":
             [3,1,2],
             [2,3,1]]
     placeholder = Board(data= data, size = 3)
-    squares = [(0,0), (1,0), (2,0)]
-    cage = Cage(board = placeholder, operator="x", goal = 6, squares=squares)
-    print(cage.verify())
+
+    test_squares = {}
+    test_cages = {}
+
+    test_squares["g"] = [(0,1)]
+    test_cages["g"] = Cage(board = placeholder, operator="g", goal = 2, squares=test_squares["g"])
+
+    test_squares["+"] = [(0, 0), (1, 1), (2, 2)]
+    test_cages["+"] = Cage(board=placeholder, operator="+", goal=3, squares=test_squares["+"])
+
+    test_squares["*"] = [(0,0), (1,0), (2,0)]
+    test_cages["*"] = Cage(board = placeholder, operator="*", goal = 6, squares=test_squares["*"])
+
+    test_squares["-"] = [(0, 2), (2, 2)]
+    test_cages["-"] = Cage(board=placeholder, operator="-", goal=2, squares=test_squares["-"])
+
+    test_squares["/"] = [(0, 1), (2, 2)]
+    test_cages["/"] = Cage(board=placeholder, operator="/", goal=2, squares=test_squares["/"])
+
+    for op in test_cages:
+        print(f"For operator {op}, verify is {test_cages[op].verify()}")
+
 
